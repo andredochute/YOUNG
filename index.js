d61f16b3e90ff79ad37a0405f788ce714c853d77
andredochute <andredochute@gmail.com>
2022-08-21 15:11:30 -0400

    Alterações

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const hbs = require('express-handlebars');
+const PORT = process.env.PORT || 3000;
+
+//configuracao do handleBars
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main'
+}));
+
+app.use('/', express.static('public'));
+app.set('view engine', 'hbs');
+
+//rota inicial
+//redenriza o home.hbs para abra dentro da tag {{{body}}} no layout
+app.get('/', (req, res)=>{
+    res.render('home');
+})
+
+app.listen(PORT, () => {
+    console.log("Servidor rodando em localhost:" + PORT)
+})
+
