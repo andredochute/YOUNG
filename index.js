f5b464eeac89eeb8a0b9556c74e23a70550d01cc
andredochute <andredochute@gmail.com>
2022-09-05 13:57:44 -0400

    Alterações

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 
 //configuracao do handleBars
@@ -10,12 +11,26 @@ app.engine('hbs', hbs.engine({
 }));
 
 app.use('/', express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}));
 app.set('view engine', 'hbs');
 
 //rota inicial
 //redenriza o home.hbs para abra dentro da tag {{{body}}} no layout
 app.get('/', (req, res)=>{
     res.render('home');
+})
+
+app.get('/cad_users', (req, res)=>{
+    res.render('cad_users');
+})
+
+app.get('/exibir_users', (req, res)=>{
+    res.render('exibir_users');
+})
+
+
+app.post('/insert_user',(req,res)=>{
+    console.log(req.body);
 })
 
 app.listen(PORT, () => {
